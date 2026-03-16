3ef6889bb8cc2f1ad1688b2f1bd8a0432350bdac
Nishant Naravarajula <naravarajula.n@northeastern.edu>
2026-03-16 12:08:55 -0400

    Setup Complete

---
+++ server/eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
