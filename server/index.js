74e6dcb98dea40027e832a7b8da6a645839fad3b
Nishant Naravarajula <naravarajula.n@northeastern.edu>
2026-03-19 10:26:57 -0400

    Final Deployment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://focusflow-1-pnq8.onrender.com",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
